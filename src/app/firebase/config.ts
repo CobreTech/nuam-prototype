d34480fd9766370e9bc9f83a0c145df06cda67f5
Benjamin Morales <129797391+RyuDakai@users.noreply.github.com>
2025-10-27 21:39:52 -0300

    v0.2: Implement user register

--- src/app/firebase/config.ts
+++ src/app/firebase/config.ts
@@ -18,6 +18,6 @@ const firebaseConfig = {
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, 'nuam');
 export const storage = getStorage(app);
 export { app };
